a99f6fb6e63bbd80a922fa1fea5473fa51ee861a
Bikram Adhikari <salik.adhikari@gmail.com>
2014-01-13 22:34:33 +0545

    first commit

---
+++ addresslib.js
@@ -0,0 +1,893 @@
+// information
+development_regions = [
+{
+	"id": "1",
+	"name": "Far-Western Development Region"
+},
+{
+	"id": "2",
+	"name": "Mid-Western Development Region"
+},
+{
+	"id": "3",
+	"name": "Western Development Region"
+},
+{
+	"id": "4",
+	"name": "Central Development Region"
+},
+{
+	"id": "5",
+	"name": "Eastern Development Region"
+}
+]
+
+zones = [
+{
+	"id": "1",
+	"name": "Mahakali"
+},
+{
+	"id": "2",
+	"name": "Seti"
+},
+{
+	"id": "3",
+	"name": "Bheri"
+},
+{
+	"id": "4",
+	"name": "Karnali"
+},
+{
+	"id": "5",
+	"name": "Rapti"
+},
+{
+	"id": "6",
+	"name": "Dhaulagiri"
+},
+{
+	"id": "7",
+	"name": "Gandaki"
+},
+{
+	"id": "8",
+	"name": "Lumbini"
+},
+{
+	"id": "9",
+	"name": "Bagmati"
+},
+{
+	"id": "10",
+	"name": "Narayani"
+},
+{
+	"id": "11",
+	"name": "Janakpur"
+},
+{
+	"id": "12",
+	"name": "Sagarmatha"
+},
+{
+	"id": "13",
+	"name": "Koshi"
+},
+{
+	"id": "14",
+	"name": "Mechi"
+}
+]
+districts = [
+{
+	"id": "1",
+	"name": "Bhaktapur"
+},
+{
+	"id": "2",
+	"name": "Dhading"
+},
+{
+	"id": "3",
+	"name": "Kathmandu"
+},
+{
+	"id": "4",
+	"name": "Kabhrepalanchok"
+},
+{
+	"id": "5",
+	"name": "Lalitpur"
+},
+{
+	"id": "6",
+	"name": "Nuwakot"
+},
+{
+	"id": "7",
+	"name": "Rasuwa"
+},
+{
+	"id": "8",
+	"name": "Sindhupalchok"
+},
+{
+	"id": "9",
+	"name": "Banke"
+},
+{
+	"id": "10",
+	"name": "Bardiya"
+},
+{
+	"id": "11",
+	"name": "Dailekh"
+},
+{
+	"id": "12",
+	"name": "Jajarkot"
+},
+{
+	"id": "13",
+	"name": "Surkhet"
+},
+{
+	"id": "14",
+	"name": "Baglung"
+},
+{
+	"id": "15",
+	"name": "Mustang"
+},
+{
+	"id": "16",
+	"name": "Myagdi"
+},
+{
+	"id": "17",
+	"name": "Parbat"
+},
+{
+	"id": "18",
+	"name": "Gorkha"
+},
+{
+	"id": "19",
+	"name": "Kaski"
+},
+{
+	"id": "20",
+	"name": "Lamjung"
+},
+{
+	"id": "21",
+	"name": "Manang"
+},
+{
+	"id": "22",
+	"name": "Syangja"
+},
+{
+	"id": "23",
+	"name": "Tanahu"
+},
+{
+	"id": "24",
+	"name": "Dhanusa"
+},
+{
+	"id": "25",
+	"name": "Dolakha"
+},
+{
+	"id": "26",
+	"name": "Mahottari"
+},
+{
+	"id": "27",
+	"name": "Ramechhap"
+},
+{
+	"id": "28",
+	"name": "Sarlahi"
+},
+{
+	"id": "29",
+	"name": "Sindhuli"
+},
+{
+	"id": "30",
+	"name": "Dolpa"
+},
+{
+	"id": "31",
+	"name": "Humla"
+},
+{
+	"id": "32",
+	"name": "Jumla"
+},
+{
+	"id": "33",
+	"name": "Kalikot"
+},
+{
+	"id": "34",
+	"name": "Mugu"
+},
+{
+	"id": "35",
+	"name": "Bhojpur"
+},
+{
+	"id": "36",
+	"name": "Dhankuta"
+},
+{
+	"id": "37",
+	"name": "Morang"
+},
+{
+	"id": "38",
+	"name": "Sankhuwasabha"
+},
+{
+	"id": "39",
+	"name": "Sunsari"
+},
+{
+	"id": "40",
+	"name": "Terhathum"
+},
+{
+	"id": "41",
+	"name": "Arghakhanchi"
+},
+{
+	"id": "42",
+	"name": "Gulmi"
+},
+{
+	"id": "43",
+	"name": "Kapilvastu"
+},
+{
+	"id": "44",
+	"name": "Nawalparasi"
+},
+{
+	"id": "45",
+	"name": "Palpa"
+},
+{
+	"id": "46",
+	"name": "Rupandehi"
+},
+{
+	"id": "47",
+	"name": "Baitadi"
+},
+{
+	"id": "48",
+	"name": "Dadeldhura"
+},
+{
+	"id": "49",
+	"name": "Darchula"
+},
+{
+	"id": "50",
+	"name": "Kanchanpur"
+},
+{
+	"id": "51",
+	"name": "Ilam"
+},
+{
+	"id": "52",
+	"name": "Jhapa"
+},
+{
+	"id": "53",
+	"name": "Panchthar"
+},
+{
+	"id": "54",
+	"name": "Taplejung"
+},
+{
+	"id": "55",
+	"name": "Bara"
+},
+{
+	"id": "56",
+	"name": "Chitwan"
+},
+{
+	"id": "57",
+	"name": "Makwanpur"
+},
+{
+	"id": "58",
+	"name": "Parsa"
+},
+{
+	"id": "59",
+	"name": "Rautahat"
+},
+{
+	"id": "60",
+	"name": "Dang Deukhuri"
+},
+{
+	"id": "61",
+	"name": "Pyuthan"
+},
+{
+	"id": "62",
+	"name": "Rolpa"
+},
+{
+	"id": "63",
+	"name": "Rukum"
+},
+{
+	"id": "64",
+	"name": "Salyan"
+},
+{
+	"id": "65",
+	"name": "Khotang"
+},
+{
+	"id": "66",
+	"name": "Okhaldhunga"
+},
+{
+	"id": "67",
+	"name": "Saptari"
+},
+{
+	"id": "68",
+	"name": "Siraha"
+},
+{
+	"id": "69",
+	"name": "Solukhumbu"
+},
+{
+	"id": "70",
+	"name": "Udayapur"
+},
+{
+	"id": "71",
+	"name": "Achham"
+},
+{
+	"id": "72",
+	"name": "Bajhang"
+},
+{
+	"id": "73",
+	"name": "Bajura"
+},
+{
+	"id": "74",
+	"name": "Doti"
+},
+{
+	"id": "75",
+	"name": "Kailali"
+}
+]
+municipalities = [
+{
+	"id": "1",
+	"name": "Kathmandu"
+},
+{
+	"id": "2",
+	"name": "Pokhara"
+},
+{
+	"id": "3",
+	"name": "Lalitpur"
+},
+{
+	"id": "4",
+	"name": "Biratnagar"
+},
+{
+	"id": "5",
+	"name": "Bharatpur"
+},
+{
+	"id": "6",
+	"name": "Birganj"
+},
+{
+	"id": "7",
+	"name": "Butwal"
+},
+{
+	"id": "8",
+	"name": "Dharan"
+},
+{
+	"id": "9",
+	"name": "Bhim Datta"
+},
+{
+	"id": "10",
+	"name": "Dhangadhi"
+},
+{
+	"id": "11",
+	"name": "Janakpur"
+},
+{
+	"id": "12",
+	"name": "Hetauda"
+},
+{
+	"id": "13",
+	"name": "Madhyapur Thimi"
+},
+{
+	"id": "14",
+	"name": "Bhaktapur"
+},
+{
+	"id": "15",
+	"name": "Damak"
+},
+{
+	"id": "16",
+	"name": "Itahari"
+},
+{
+	"id": "17",
+	"name": "Nepalganj"
+},
+{
+	"id": "18",
+	"name": "Triyuga"
+},
+{
+	"id": "19",
+	"name": "Kirtipur"
+},
+{
+	"id": "20",
+	"name": "Siddharthanagar"
+},
+{
+	"id": "21",
+	"name": "Ghorahi"
+},
+{
+	"id": "22",
+	"name": "Lekhnath"
+},
+{
+	"id": "23",
+	"name": "Mechinagar"
+},
+{
+	"id": "24",
+	"name": "Tikapur"
+},
+{
+	"id": "25",
+	"name": "Gulariya"
+},
+{
+	"id": "26",
+	"name": "Tulsipur"
+},
+{
+	"id": "27",
+	"name": "Birendranagar"
+},
+{
+	"id": "28",
+	"name": "Ratnanagar"
+},
+{
+	"id": "29",
+	"name": "Byas"
+},
+{
+	"id": "30",
+	"name": "Kalaiya"
+},
+{
+	"id": "31",
+	"name": "Kamalamai"
+},
+{
+	"id": "32",
+	"name": "Rajbiraj"
+},
+{
+	"id": "33",
+	"name": "Gaur"
+},
+{
+	"id": "34",
+	"name": "Lahan"
+},
+{
+	"id": "35",
+	"name": "Gorkha"
+},
+{
+	"id": "36",
+	"name": "Putalibazar"
+},
+{
+	"id": "37",
+	"name": "Kapilvastu"
+},
+{
+	"id": "38",
+	"name": "Baglung"
+},
+{
+	"id": "39",
+	"name": "Tansen"
+},
+{
+	"id": "40",
+	"name": "Inaruwa"
+},
+{
+	"id": "41",
+	"name": "Siraha"
+},
+{
+	"id": "42",
+	"name": "Panauti"
+},
+{
+	"id": "43",
+	"name": "Bidur"
+},
+{
+	"id": "44",
+	"name": "Dhankuta"
+},
+{
+	"id": "45",
+	"name": "Khandbari"
+},
+{
+	"id": "46",
+	"name": "Ramgram"
+},
+{
+	"id": "47",
+	"name": "Malangwa"
+},
+{
+	"id": "48",
+	"name": "Banepa"
+},
+{
+	"id": "49",
+	"name": "Waling"
+},
+{
+	"id": "50",
+	"name": "Jaleswar"
+},
+{
+	"id": "51",
+	"name": "Dipayal-Silgadhi"
+},
+{
+	"id": "52",
+	"name": "Bhimeshwar"
+},
+{
+	"id": "53",
+	"name": "Amaragadhi"
+},
+{
+	"id": "54",
+	"name": "Narayan"
+},
+{
+	"id": "55",
+	"name": "Ilam"
+},
+{
+	"id": "56",
+	"name": "Bhadrapur"
+},
+{
+	"id": "57",
+	"name": "Dasharathchand"
+},
+{
+	"id": "58",
+	"name": "Dhulikhel"
+},
+{
+	"id": "59",
+	"name": "Bandipur"
+},
+{
+	"id": "60",
+	"name": "Beni"
+},
+{
+	"id": "61",
+	"name": "Besisahar"
+},
+{
+	"id": "62",
+	"name": "Bhojpur"
+},
+{
+	"id": "63",
+	"name": "Birtamod"
+},
+{
+	"id": "64",
+	"name": "Chainpur"
+},
+{
+	"id": "65",
+	"name": "Champapur (Chapagaun)"
+},
+{
+	"id": "66",
+	"name": "Chandanath"
+},
+{
+	"id": "67",
+	"name": "Chandranigahapur"
+},
+{
+	"id": "68",
+	"name": "Chautara"
+},
+{
+	"id": "69",
+	"name": "Dhanushadham"
+},
+{
+	"id": "70",
+	"name": "Diktel"
+},
+{
+	"id": "71",
+	"name": "Gaidakot"
+},
+{
+	"id": "72",
+	"name": "Hariwan"
+},
+{
+	"id": "73",
+	"name": "Jitpur"
+},
+{
+	"id": "74",
+	"name": "Kanchanpur Rupnagar"
+},
+{
+	"id": "75",
+	"name": "Karyabinayak"
+},
+{
+	"id": "76",
+	"name": "Katari"
+},
+{
+	"id": "77",
+	"name": "Khairenitar Dulegauda"
+},
+{
+	"id": "78",
+	"name": "Kohalpur"
+},
+{
+	"id": "79",
+	"name": "Kusma"
+},
+{
+	"id": "80",
+	"name": "Mangalsen"
+},
+{
+	"id": "81",
+	"name": "Manthali"
+},
+{
+	"id": "82",
+	"name": "Mohanpur-Kalyanpur"
+},
+{
+	"id": "83",
+	"name": "Myanglung"
+},
+{
+	"id": "84",
+	"name": "Nigaul Gaushala"
+},
+{
+	"id": "85",
+	"name": "Nilkantha"
+},
+{
+	"id": "86",
+	"name": "Panchkhal"
+},
+{
+	"id": "87",
+	"name": "Phidim"
+},
+{
+	"id": "88",
+	"name": "Pyuthan"
+},
+{
+	"id": "89",
+	"name": "Rajapur"
+},
+{
+	"id": "90",
+	"name": "Ramnagar Mirchaiya"
+},
+{
+	"id": "91",
+	"name": "Sakhuwa Mahendranagar"
+},
+{
+	"id": "92",
+	"name": "Salyan"
+},
+{
+	"id": "93",
+	"name": "Sandhikharka"
+},
+{
+	"id": "94",
+	"name": "Sanphebagar"
+},
+{
+	"id": "95",
+	"name": "Sankharpur"
+},
+{
+	"id": "96",
+	"name": "Sunwal"
+},
+{
+	"id": "97",
+	"name": "Tamghas"
+},
+{
+	"id": "98",
+	"name": "Taplejung"
+},
+{
+	"id": "99",
+	"name": "Urlabari"
+}
+]
+
+// these three arrays form the relation ship between development region, zones, districts and municipalities of our country
+var development_region_Zone_Array = [[1,2],[3,4,5],[6,7,8],[9,10,11],[12,13,14]]
+var zones_district_Array = [[47,48,49,50],[71,72,73,74,75],[9,10,11,12,13],[30,31,32,33,34],[60,61,62,63,64],[14,15,16,17],[18,19,20,21,22,23],[41,42,43,44,45,46],[1,2,3,4,5,6,7,8],[55,56,57,58,59],[24,25,26,27,28,29],[65,66,67,68,69,70],[35,36,37,38,39,40],[51,52,53,54]];
+var zones_municipalities_Array = [[13,14],[85],[1,19,95],[42,48,58,86],[3,65,75],[43],[],[68],[17,78,88],[25,89],[54],[],[27],[38],[],[60],[79],[35],[2,22],[61],[],[36,49],[29,59,77],[11,69,91],[52],[50,84],[81],[47,72],[31],[],[],[66],[],[],[62],[44],[4,99],[45],[8,16,40],[83],[93],[97],[37],[46,71,96],[39],[7,20],[57],[53],[],[9],[55],[15,23,56,63],[87,98],[],[30,73],[5,28],[12],[6],[33,67],[21,26],[],[],[],[64],[],[70],[],[32,74,82],[34,41,90],[],[18,76],[80,94],[64],[],[51],[10,24]];
+
+function getZoneIDs(development_regionID) {
+	return development_region_Zone_Array[development_regionID-1]
+}
+
+function getDistrictsIDs(zoneID){
+	return zones_district_Array[zoneID-1]
+}
+
+function getMunicipalitiesIDs(districtID){
+	return zones_municipalities_Array[districtID-1]
+}
+
+function NameArrayfromIDArray(IDs,infoArray)
+{
+	
+	var names = [];
+	for (var keys in infoArray)
+	{
+		
+		if (IDs.indexOf(parseInt(infoArray[keys].id)) !== -1){
+			names.push(infoArray[keys])
+		}
+	}
+	return names
+
+
+
+}
+
+
+function update()
+{
+	
+	switch(this.id)
+	{
+		case "developmentRegion":
+		removeOptions("Zones")
+		var selectd =  document.getElementById("Zones")
+		var idOfAltered = document.getElementById("developmentRegion").value
+		var IDs = getZoneIDs(idOfAltered)
+		var zoneNames = NameArrayfromIDArray(IDs,zones)	
+		break;
+
+		case "Zones":
+		removeOptions("District")
+		var selectd =  document.getElementById("District")
+		var idOfAltered = document.getElementById("Zones").value
+		var IDs = getDistrictsIDs(idOfAltered)
+		var zoneNames = NameArrayfromIDArray(IDs,districts)	
+		break;
+
+		case "District":
+		removeOptions("Municipalities")
+		var selectd =  document.getElementById("Municipalities")
+		var idOfAltered = document.getElementById("District").value
+		var IDs = getMunicipalitiesIDs(idOfAltered)
+		var zoneNames = NameArrayfromIDArray(IDs,municipalities)	
+		break;
+		
+		
+	}
+	for (var keys in zoneNames)
+	{
+
+		option = createOption(zoneNames[keys].id,zoneNames[keys].name)
+		selectd.appendChild(option)
+	}
+}
+
+
+function removeOptions(select)
+{
+	selectbox = document.getElementById(select)
+	var i;
+	for(i=selectbox.options.length-1;i>=0;i--)
+	{
+		selectbox.remove(i);
+
+	}
+	// lets add the header
+	option = createOption(0,select)
+	selectbox.appendChild(option)
+}
+function createOption(id, content)
+{
+	var option = document.createElement("option");
+	option.value = id
+	option.innerHTML = content
+
+	return option
+}
+
+window.onload = function () {
+	var select_development_regions =  document.getElementById("developmentRegion")
+	for (var keys in development_regions)
+		{			option = createOption(development_regions[keys].id,development_regions[keys].name)
+			select_development_regions.appendChild(option)
+		}
+		
+	}
+	document.getElementById('developmentRegion').addEventListener('change',update,false);
+	document.getElementById('Zones').addEventListener('change',update,false);
+	document.getElementById('District').addEventListener('change',update,false);
+
+
